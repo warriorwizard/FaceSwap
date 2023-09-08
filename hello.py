17be4cd3bb44b506112f427d1c9c2713ea1b8c19
Amit Yadav <amit375395yadav@gmail.com>
2023-09-08 06:49:57 +0530

    added

---
+++ hello.py
@@ -0,0 +1,6 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import insightface
+from insightface.app import FaceAnalysis
+from insightface.data import get_image as ins_get_image
